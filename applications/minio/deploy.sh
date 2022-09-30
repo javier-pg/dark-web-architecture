daebb17fa3d9c8e1c73b849efe8ccc97eb4c6584
javier <javier@javierpg@um.es>
2022-09-30 08:53:50 +0200

    Implementation of crawlab and spider

--- applications/minio/deploy.sh
+++ applications/minio/deploy.sh
@@ -1,5 +1,5 @@
 sed -i 's/MINIO_PASSWORD/'"$MINIO_PASSWORD"'/' minio-dev.yaml
 sed -i 's/MINIO_USER/'"$MINIO_USER"'/' minio-dev.yaml
 kubectl apply -f minio-dev.yaml
-sed -i 's/'"$MINIO_PASSWORD"'/MINIO_PASSWORD/' minio/minio-dev.yaml
-sed -i 's/'"$MINIO_PASSWORD"'/MINIO_USER/' minio/minio-dev.yaml
+sed -i 's/'"$MINIO_PASSWORD"'/MINIO_PASSWORD/' minio-dev.yaml
+sed -i 's/'"$MINIO_PASSWORD"'/MINIO_USER/' minio-dev.yaml
