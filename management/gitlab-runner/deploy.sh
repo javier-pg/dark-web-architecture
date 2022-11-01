42db4d5e40aad8a0dd6a69b5fa6d2f82930a0972
javier <javier@javierpg@um.es>
2022-11-01 17:35:26 +0100

    Fixing runner

--- management/gitlab-runner/deploy.sh
+++ management/gitlab-runner/deploy.sh
@@ -3,3 +3,4 @@ kubectl create namespace gitlab-runner
 kubectl create secret generic gitlab-runner-secret --namespace gitlab-runner --from-file=/etc/gitlab/ssl/10.10.10.111.crt
 #sudo helm repo add gitlab https://charts.gitlab.io
 sudo helm install --namespace gitlab-runner gitlab-runner --set certsSecretName=gitlab-runner-secret --set gitlabUrl=$GITLAB_URL --set runnerRegistrationToken=$GITLAB_RUNNER_TOKEN --set rbac.create="true" gitlab/gitlab-runner
+kubectl create clusterrolebinding gitlab-runner-binding --clusterrole=cluster-admin --serviceaccount=gitlab-runner:default
