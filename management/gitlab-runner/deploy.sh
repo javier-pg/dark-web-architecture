a2e82e4317448faaa293161538d6dbc649cad29b
javier <javier@javierpg@um.es>
2022-11-02 14:59:07 +0100

    Kafka CI/CD

--- management/gitlab-runner/deploy.sh
+++ management/gitlab-runner/deploy.sh
@@ -2,5 +2,5 @@ source ../local.env
 kubectl create namespace gitlab-runner
 kubectl create secret generic gitlab-runner-secret --namespace gitlab-runner --from-file=/etc/gitlab/ssl/10.10.10.111.crt
 #sudo helm repo add gitlab https://charts.gitlab.io
-sudo helm install --namespace gitlab-runner gitlab-runner --set certsSecretName=gitlab-runner-secret --set gitlabUrl=$GITLAB_URL --set runnerRegistrationToken=$GITLAB_RUNNER_TOKEN --set rbac.create="true" gitlab/gitlab-runner
+sudo helm install --namespace gitlab-runner gitlab-runner --set certsSecretName=gitlab-runner-secret --set gitlabUrl=$GITLAB_URL --set runnerRegistrationToken=$GITLAB_RUNNER_TOKEN --set rbac.create="true" --values values.yaml gitlab/gitlab-runner
 kubectl create clusterrolebinding gitlab-runner-binding --clusterrole=cluster-admin --serviceaccount=gitlab-runner:default
